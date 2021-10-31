9731d22c01f9511747c10321c61ad115bd67324d
Vikmcr99 <victor.robinson@al.infnet.edu.br>
2021-10-31 12:12:22 -0300

    Main Reajustado

--- Victor_Robinson_TP1C/Program.cs
+++ Victor_Robinson_TP1C/Program.cs
@@ -2,49 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 
-// Aqui foi minha tentativa de fazer orientado a objeto, mas acabei fazendo de acordo com o enunciado do TP
-
-
 namespace Victor_Robinson_TP1C
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Menu();
+            Console.WriteLine("TP01 C# - Calculadora básia dividia em 4 projetos - Victor Robinson");
         }
-
-        public static void Menu()
-        {
-            Console.WriteLine("Escolha uma operação abaixo:");
-            Console.WriteLine("1 - Somar\n2 - Subtrair\n3 - Multiplicar\n4 - Dividir\n5 - Sair");
-
-            string escolha = Console.ReadLine();
-
-            // Optei pelo If ao inves do Switch
-
-            //if (escolha == "1")
-            //{
-            //    Soma();
-            //}
-            //if (escolha == "2")
-            //{
-            //    Subtracao();
-            //}
-            //if (escolha == "3")
-            //{
-            //    Multiplicar();
-            //}
-            //if (escolha == "4")
-            //{
-            //    Dividir();
-            //}
-            //if (escolha == "5")
-            //{
-            //    Console.WriteLine("Saindo...");
-            //    Environment.Exit(0);
-            //}
-        }
-
     }
 }
